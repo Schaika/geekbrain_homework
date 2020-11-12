7f9b216ff68910107834c89db2f32ccf2e570e54
Viktor <nik100n@gmail.com>
2020-11-12 19:12:29 +0300

    Update main.cpp

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -29,6 +29,7 @@ class Card{
 	enum::rank m_Rank;
 	enum::suit m_Suit;
 	bool m_IsFaceUp;
+	friend ostream& operator<<(ostream& os, const Card& aCard);
 public:
 	Card(int r,int s, bool IFU): m_Rank(static_cast<enum::rank>(r)), m_Suit(static_cast<enum::suit>(s)), m_IsFaceUp(IFU) {}
 	int GetValue(){
@@ -37,7 +38,25 @@ public:
 	void Flip(){
 			m_IsFaceUp=!m_IsFaceUp;
 		}
+
 };
+ostream& operator<<(ostream& os, const Card& aCard)
+{
+	const string RANKS[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9","10", "J", "Q", "K" };
+	const string SUITS[] = { "C", "D", "H", "S" };
+
+	if (aCard.m_IsFaceUp)
+	{
+		os << RANKS[static_cast<int>(aCard.m_Rank)] << SUITS[static_cast<int>(aCard.m_Suit)];
+	}
+	else
+	{
+		os << "XX";
+	}
+
+	return os;
+}
+
 
 class Hand{
 protected:
@@ -83,24 +102,113 @@ public:
 			}
 			return total;
 		}
-	~Hand(){
+	virtual ~Hand(){
 			Clear();
 		}
 
 };
 
 class GenericPlayer : public Hand{
+	friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
 protected:
-	string name;
+	string m_Name;
 public:
-	virtual bool IsHitting()=0;
+	GenericPlayer(const string& name) : m_Name(name){};
+
+	virtual bool IsHitting() const = 0;
+
 	bool IsBoosted() const{
 			return this->GetTotal()>21;
 		}
 	void Bust() const{
-                cout<<name<<" is busted."<<endl;
+				cout<<m_Name<<" is busted."<<endl;
 	}
+	virtual ~GenericPlayer(){};
 	};
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+	os << aGenericPlayer.m_Name << ":\t";
+
+	vector<Card*>::const_iterator pCard;
+	if (!aGenericPlayer.m_Cards.empty())
+	{
+		for (pCard = aGenericPlayer.m_Cards.begin(); pCard != aGenericPlayer.m_Cards.end(); pCard++)
+		{
+			os << *(*pCard) << "\t";
+		}
+
+
+		if (aGenericPlayer.GetTotal() != 0)
+		{
+			cout << "(" << aGenericPlayer.GetTotal() << ")";
+		}
+	}
+	else
+	{
+		os << "<empty>";
+	}
+
+	return os;
+}
+
+class Player : public GenericPlayer
+{
+public:
+	Player(const string& name = "");
+
+	// показывает, хочет ли игрок продолжать брать карты
+	bool IsHitting() const
+		{
+			cout << m_Name << ", do you want a hit? (Y/N): ";
+			char response;
+			cin >> response;
+			return (response == 'y' || response == 'Y');
+		}
+
+
+	// объявляет, что игрок победил
+	void Win() const{
+			cout << m_Name << " wins.\n";
+		}
+
+	// объявляет, что игрок проиграл
+	void Lose() const{
+			cout << m_Name << " loses.\n";
+		}
+
+	// объявляет ничью
+	void Push() const{
+			cout << m_Name << " pushes.\n";
+		}
+
+	~Player();
+};
+
+class House : public GenericPlayer
+{
+public:
+	House(const string& name = "House");
+
+	virtual ~House();
+
+	// показывает, хочет ли дилер продолжать брать карты
+	bool IsHitting() const{
+			return (GetTotal() <= 16);
+		}
+
+	// переворачивает первую карту
+	void FlipFirstCard(){
+			if (!(m_Cards.empty()))
+			{
+				m_Cards[0]->Flip();
+			}
+			else
+			{
+				cout << "No card to flip!\n";
+			}
+		}
+};
+
 
 int main()
 {
